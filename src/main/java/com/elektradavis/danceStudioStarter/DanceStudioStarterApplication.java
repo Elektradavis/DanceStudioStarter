913ba28adae11d413343785c25c4049d9be81b01
Elektra Davis <elektradavis117@gmail.com>
2023-05-15 11:11:58 -0400

    first commit

---
+++ src/main/java/com/elektradavis/danceStudioStarter/DanceStudioStarterApplication.java
@@ -0,0 +1,13 @@
+package com.elektradavis.danceStudioStarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanceStudioStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanceStudioStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elektradavis/danceStudioStarter/DanceStudioStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elektradavis.danceStudioStarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanceStudioStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
